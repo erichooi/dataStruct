3aace1bdc1fc86ed98ea85d5abf0b4f02d1b952d
Eric Khoo Jiun Hooi <erichooi1995@gmail.com>
2016-10-19 21:44:19 +0800

    Add quicksort and delete bubblesort for array

--- sort.cpp
+++ sort.cpp
@@ -2,41 +2,18 @@
 #include <algorithm>
 #include <vector>
 
-// bubblesort for array
-/*
-void bubbleSort(int *arr, int length)
-{
-	int i, j;
-	bool isSorted = false;
-	for (i = 0; (i < length-1) && (isSorted==false); i++) {
-		isSorted = true;
-		for (j = 0; j < length-i-1; j++) {
-			if (arr[j] > arr[j+1]) {
-				std::swap(arr[j], arr[j+1]);
-				isSorted = false;
-			}
-		}
-	}
-}
-*/
-
-void displaySwap(std::vector<int>&vector) {
-	std::cout << "Swapping is happening: ";
-	for(std::vector<int>::iterator itr = vector.begin(); itr != vector.end(); itr++) {
-		std::cout << *itr << " ";
-	}
-	std::cout << "\n";
-}
 
 // Simple Sort
 
 // bubble sort for vector
-void bubbleSort(std::vector<int> &vector)
+template <typename Comparable>
+void bubbleSort(std::vector<Comparable> &vector)
 {
-	int i, j;
+	int i;
+	int j;
 	bool isSorted = false; // flag to stop iteration if it is sorted
 	for (i = 0; (i < vector.size()-1) && (isSorted==false); i++) {
-		isSorted = true;
+        isSorted = true;
 		for (j = 0; j < vector.size()-i-1; j++) {
 			if (vector[j] > vector[j+1]) {
 				std::swap(vector[j], vector[j+1]);
@@ -48,7 +25,8 @@ void bubbleSort(std::vector<int> &vector)
 }
 
 // selection sort for vector
-void selectionSort(std::vector<int> &vector) 
+template <typename Comparable>
+void selectionSort(std::vector<Comparable> &vector) 
 {
 	int i, j, min, minIndex;
 	for (i = 0; i < vector.size(); i++) {
@@ -61,19 +39,19 @@ void selectionSort(std::vector<int> &vector)
 			}
 		}
 		std::swap(vector[i], vector[minIndex]);
-		displaySwap(vector);
 	}
 }
 
 // insertion sort for vector
-void insertionSort(std::vector<int> &vector)
+template <typename Comparable>
+void insertionSort(std::vector<Comparable> &vector)
 {
-	int i, j;
+	int i;
+	int j;
 	for (i = 1; i < vector.size(); i++) {
 		j = i;
 		while (j > 0 && vector[j-1] > vector[j]) {
 			std::swap(vector[j], vector[j-1]);
-			displaySwap(vector);
 			j--;
 		}
 	}
@@ -88,7 +66,7 @@ template <typename Comparable>
 void merge(std::vector<Comparable> &, std::vector<Comparable> &, int, int, int);
 
 /**
- * Merge Sort algorithm
+ * Merge Sort algorithm (driver)
  */
 template <typename Comparable>
 void mergeSort(std::vector<Comparable> &a)
@@ -155,14 +133,71 @@ void merge(std::vector<Comparable> &a, std::vector<Comparable> &tmpArray,
 }
 
 
+// quicksort
+// ** cannot compare with the same number in vector (bug)
+
+template <typename Comparable>
+void quickSort(std::vector<Comparable> &, int, int);
+template <typename Comparable>
+int partition(std::vector<Comparable> &, int, int);
+
+/**
+ * Quicksort algorithm (driver)
+ */
+template <typename Comparable>
+void quickSort(std::vector<Comparable> &a)
+{
+    quickSort(a, 0, a.size() - 1);
+}
+
+template <typename Comparable>
+void quickSort(std::vector<Comparable> &a, int first, int last) 
+{
+    int cut;
+    if (first < last) {
+        cut = partition(a, first, last);
+        quickSort(a, first, cut);
+        quickSort(a, cut+1, last);
+    }
+}
+
+template <typename Comparable>
+int partition(std::vector<Comparable> &a, int first, int last) 
+{
+    int pivot, temp;
+    int loop, cutPoint, bottom, top;
+    pivot = a[first];
+    bottom = first;
+    top = last;
+    loop = 1;
+
+    while (loop) {
+        while (a[top] > pivot) {
+            top--;
+        }
+        while (a[bottom] < pivot) {
+            bottom++;
+        }
+        if (bottom < top) {
+            std::swap(a[bottom], a[top]);
+        } else {
+            loop = 0;
+            cutPoint = top;
+        }
+    }
+    return cutPoint;
+}
+
+
+// main function
 int main() {
-	std::vector<int> vector {2,5,6,10,0,9,7};
+	std::vector<int> vector {70,15,85,25,8,16,22,35};
 	std::cout << "Before Sort: ";
 	for (std::vector<int>::iterator itr=vector.begin(); itr < vector.end(); itr++) {
 		std::cout << *itr << " ";
 	}
 	std::cout << "\n";
-	mergeSort(vector);	
+	quickSort(vector);	
 	std::cout << "After Sort: ";
 	for (std::vector<int>::iterator itr=vector.begin(); itr < vector.end(); itr++) {
 		std::cout << *itr << " ";
